5d69db00ed7c25ec2e017343922787cf6ad5b9ba
Ricardo Brites <93977728+Ricard0Brites@users.noreply.github.com>
2025-08-30 04:49:27 +0100

    Removed Route

--- src/App.jsx
+++ src/App.jsx
@@ -63,7 +63,7 @@ export default class App extends Component
         <meta name="viewport" content="width=device-width, initial-scale=1"/>  
       </header>
       <Routes>
-        <Route path="/RicardoBrites" element={this.GetFrontEnd()} />
+        <Route path="/" element={this.GetFrontEnd()} />
         <Route path="Development/" element={this.GetDevelopment()} />
       </Routes>
 
