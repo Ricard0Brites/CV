e28c67c27b5d6cc920c3f1bbf58d11f6fbb79b6e
Ricardo Brites <93977728+Ricard0Brites@users.noreply.github.com>
2025-08-30 02:12:52 +0100

    Added Print Safety

--- src/App.jsx
+++ src/App.jsx
@@ -119,84 +119,93 @@ export default class App extends Component
         sm:pl-10
         sm:pr-10
         pl-5'>
-
-          <AboutMeSection Title={"About Me"} Name={'Ricardo Brites - 24'} PhotoPath={'src\\assets\\RicardoBritesPhoto.png'} Description={'Adaptable C++ developer focused on clean, maintainable systems. Passionate about learning, problem-solving, and contributing to meaningful game experiences.'}/>
-          <ExperienceSection Title={"Experience"} Objects=
-          {
-            [ 
-              this.#ExperienceEntryObject("Fracture Labs", "https://decimated.net/", "Game Engineer", "Remote", "2022-2025", 
-                `
-                  Led development of the user interface for the game Decimated.
-                  Built and maintained backend systems, optimized existing codebases, conducted technical research, produced internal documentation,
-                  debugged and resolved complex issues, and managed user account systems via PlayFab.
-                `),
-              this.#ExperienceEntryObject("Arxi", "https://www.arxi.pt/en_US", "Web Development Intern", "Leiria - Portugal", "2019-2019", "Designed and developed a fully functional web application using Django.")
-            ]
-          }/>
-
-          <ExperienceSection Title={"Education"} Objects=
-          {
-            [ 
-              this.#ExperienceEntryObject("Polytechnic Institute of Leiria School of Technology and Management", "https://www.ipleiria.pt/curso/licenciatura-em-jogos-digitais-e-multimedia/", "Bachelor's Degree in Game Development", "Leiria - Portugal", "2020-2024", "Practice-focused degree covering game programming, computer graphics, game design, and AI with industry tools such as Unreal Engine, Unity, and C++"),
-              this.#ExperienceEntryObject("Dominguos Sequeira High School", "http://www.esds.edu.pt/images/ano20232024/oferta_formativa_CP_2024_25.pdf#page=12&zoom=150%", "Computer Systems Management and Programming", "Leiria - Portugal", "2016-2019", "Technical secondary education focused on software development, system administration, and database management. Covered programming fundamentals (C, VB, SQL), network configuration, operating systems, and IT project workflows.")
-            ]
-          } />
-
-          <RelocationSection Title={'Relocation & Work Authorization'}/>
-
-          
-          <div className='print-only'>
-          <br />
-          <br />
-          <br />
-          <br />
+          <div className='break-inside-avoid'>
+            <AboutMeSection 
+              Title={"About Me"}
+              Name={'Ricardo Brites - 24'}
+              PhotoPath={'src\\assets\\RicardoBritesPhoto.png'}
+              Description={'Adaptable C++ developer focused on clean, maintainable systems. Passionate about learning, problem-solving, and contributing to meaningful game experiences.'}
+            />
           </div>
 
-          <ExperienceSection Title={"Communication"} Objects=
-          {
-            [ 
-              this.#ExperienceEntryObject("Portuguese","", "Native", "", "", ""),
-              this.#ExperienceEntryObject("English", "", "Fluent", "CEFR C2", "", ""),
-            ]
-          } />
+          <div className='break-inside-avoid'>
+            <ExperienceSection Title={"Experience"} Objects=
+            {
+              [ 
+                this.#ExperienceEntryObject("Fracture Labs", "https://decimated.net/", "Game Engineer", "Remote", "2022-2025", 
+                  `
+                    Led development of the user interface for the game Decimated.
+                    Built and maintained backend systems, optimized existing codebases, conducted technical research, produced internal documentation,
+                    debugged and resolved complex issues, and managed user account systems via PlayFab.
+                  `),
+                this.#ExperienceEntryObject("Arxi", "https://www.arxi.pt/en_US", "Web Development Intern", "Leiria - Portugal", "2019-2019", "Designed and developed a fully functional web application using Django.")
+              ]
+            }/>
+          </div>
+          <div className='break-inside-avoid'>
+            <ExperienceSection Title={"Education"} Objects=
+            {
+              [ 
+                this.#ExperienceEntryObject("Polytechnic Institute of Leiria School of Technology and Management", "https://www.ipleiria.pt/curso/licenciatura-em-jogos-digitais-e-multimedia/", "Bachelor's Degree in Game Development", "Leiria - Portugal", "2020-2024", "Practice-focused degree covering game programming, computer graphics, game design, and AI with industry tools such as Unreal Engine, Unity, and C++"),
+                this.#ExperienceEntryObject("Dominguos Sequeira High School", "http://www.esds.edu.pt/images/ano20232024/oferta_formativa_CP_2024_25.pdf#page=12&zoom=150%", "Computer Systems Management and Programming", "Leiria - Portugal", "2016-2019", "Technical secondary education focused on software development, system administration, and database management. Covered programming fundamentals (C, VB, SQL), network configuration, operating systems, and IT project workflows.")
+              ]
+            } />
+          </div>
+          <div className='break-inside-avoid'>
+            <RelocationSection Title={'Relocation & Work Authorization'}/>
+          </div>
 
-          <SkillsSection 
-            Title={"Skills"} 
-            Data = {SkillsList} 
-          />
-
-          <ProjectSection Title="Projects"  Objects=
-          {
-            [
-              this.#ProjectEntryObject(
-                "src/assets/GameEngineProjectCover.png",
-                "C++ Game Engine",
-                `
-                Custom lightweight 2D focused C++ Game Engine based on OpenGL
-                <br></br>Built core systems: rendering pipeline, event system, input handling, collision detection, and entity management.
-                <br></br>Optimized rendering for real-time performance, achieving 1500+ FPS uncapped in test environments.
-                <br></br>Focused on modularity and maintainability for scalability.
-                `,
-                "https://github.com/Ricard0Brites/Game-Engine/tree/main"),
+          <div className='break-inside-avoid'>
+            <ExperienceSection Title={"Communication"} Objects=
+            {
+              [ 
+                this.#ExperienceEntryObject("Portuguese","", "Native", "", "", ""),
+                this.#ExperienceEntryObject("English", "", "Fluent", "CEFR C2", "", ""),
+              ]
+            } />
+          </div>
+          
+          <div className='break-inside-avoid'>
+            <SkillsSection 
+              Title={"Skills"} 
+              Data = {SkillsList} 
+            />
+          </div>
+          
+          <div className='break-inside-avoid'>
+            <ProjectSection Title="Projects"  Objects=
+            {
+              [
                 this.#ProjectEntryObject(
-                "src/assets/CVWebsiteProjectCover.png",
-                "Curriculum Website",
-                `
-                  <br></br>Developed a responsive single-page portfolio showcasing projects, skills, and experience.
-                  <br></br>Implemented React components styled with TailwindCSS.
-                `,
-                "https://github.com/Ricard0Brites/cv-website/tree/one-pager"),
-              this.#ProjectEntryObject(
-                "src/assets/JSONEditorProjectCover.png",
-                "WPF JSON Editor (MVVM)",
-                `
-                JSON Editor application built with WPF in C# using the MVVM pattern.
-                <br></br>Implements data binding, commands, and observable collections for responsive UI updates.
-                <br></br>Supports creating, editing, and validating JSON structures with real-time feedback.
-                `,
-                "https://github.com/Ricard0Brites/JSON_Editor/tree/main")
-            ]
-          } />
+                  "src/assets/GameEngineProjectCover.png",
+                  "C++ Game Engine",
+                  `
+                  Custom lightweight 2D focused C++ Game Engine based on OpenGL
+                  <br></br>Built core systems: rendering pipeline, event system, input handling, collision detection, and entity management.
+                  <br></br>Optimized rendering for real-time performance, achieving 1500+ FPS uncapped in test environments.
+                  <br></br>Focused on modularity and maintainability for scalability.
+                  `,
+                  "https://github.com/Ricard0Brites/Game-Engine/tree/main"),
+                  this.#ProjectEntryObject(
+                  "src/assets/CVWebsiteProjectCover.png",
+                  "Curriculum Website",
+                  `
+                    <br></br>Developed a responsive single-page portfolio showcasing projects, skills, and experience.
+                    <br></br>Implemented React components styled with TailwindCSS.
+                  `,
+                  "https://github.com/Ricard0Brites/cv-website/tree/one-pager"),
+                this.#ProjectEntryObject(
+                  "src/assets/JSONEditorProjectCover.png",
+                  "WPF JSON Editor (MVVM)",
+                  `
+                  JSON Editor application built with WPF in C# using the MVVM pattern.
+                  <br></br>Implements data binding, commands, and observable collections for responsive UI updates.
+                  <br></br>Supports creating, editing, and validating JSON structures with real-time feedback.
+                  `,
+                  "https://github.com/Ricard0Brites/JSON_Editor/tree/main")
+              ]
+            } />
+          </div>
         </div>
       </div>
     );
